fce700e7cd89f94a76bcf733927364e2095e0d4f
pekanka <justshura11@gmail.com>
2021-10-13 16:06:46 +0300

    Added working with console. test renamed to main

---
+++ main.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn import linear_model
+from sklearn import model_selection
+from sklearn import preprocessing
+from sklearn import metrics
+from sklearn import cluster
+from html_parser import pars
+import data_prep
+from pr_destribute import pr_dest
+import _pickle as cPickle
+import pred
+import typer
+
+app = typer.Typer()
+
+@app.command()
+def main(link: str):
+    data = np.array(list(pars(link).values()))
+    with open("pr_selector.pkl", "rb") as f:
+        slc = cPickle.load(f)
+    data, tar = data_prep.prepare(data)
+    data = data.astype(float)
+    typer.echo("Predicted price:" + str(pred.pr_price(data)[0]))
+    typer.echo("Real price:" + str(tar[0][0]))
+
+if __name__ == "__main__":
+    app()
